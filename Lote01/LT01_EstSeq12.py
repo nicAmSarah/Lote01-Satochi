c7dacc8542ca3892c463ba574b060755e14bf570
nicAmSarah <nicaciosarah085@gmail.com>
2025-02-25 09:38:51 -0300

    Ex12 - Lote01

---
+++ Lote01/LT01_EstSeq12.py
@@ -0,0 +1,8 @@
+ano_nascimento = int(input("Digite o ano do seu nascimento"))
+ano_atual = int(input("Digite o ano atual"))
+
+idade = ano_atual - ano_nascimento
+
+daqui_a_17anos = idade + 17
+
+print(f"Sua idade atual é {idade} e daqui a 17 anos você estará com {daqui_a_17anos}")
