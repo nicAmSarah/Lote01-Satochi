2e07e35b8ddd8c8d2bb775dc72288db8e5dc324f
nicAmSarah <nicaciosarah085@gmail.com>
2025-02-25 09:07:52 -0300

    Ex01 - Lote09

---
+++ Lote01/LT01_EstSeq09.py
@@ -0,0 +1,6 @@
+valor1 = int(input("digite o primeiro valor"))
+valor2 = int(input("digite o segundo valor"))
+
+diferença = valor1 - valor2
+
+print(f"A diferença do valor {valor1} do {valor2} é igual a {diferença}")
