79592f84cb0961ce961207bea9a531dd5406d65e
nicAmSarah <nicaciosarah085@gmail.com>
2025-02-25 08:42:28 -0300

    Ex01 - Lote05

---
+++ Lote01/LT01.EstSeq05.py
@@ -0,0 +1,11 @@
+coeA = float(input("digite o valor do coeficienteA de uma equação do segundo grau.\n"))
+coeB = float(input("digite o valor do coeficienteB de uma equação do segundo grau.\n"))
+coeC = float(input("digite o valor do coeficienteC de uma equação do segundo grau.\n"))
+
+delta = (coeB*coeB - (4*coeA*coeC))
+
+raizP = (-(coeB) + delta**0.5)/2*coeA
+
+raizN = (-(coeB) - delta**0.5)/2*coeA
+
+print(raizP,raizN)
