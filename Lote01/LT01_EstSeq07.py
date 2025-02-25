eb5579c725d94940720246e26ed79233ee3c76f6
nicAmSarah <nicaciosarah085@gmail.com>
2025-02-25 08:58:24 -0300

    Ex01 - Lote07

---
+++ Lote01/LT01_EstSeq07.py
@@ -0,0 +1,7 @@
+comprimento = float(input("informe o comprimento do paralelepipedo"))
+altura = float(input("informe a altura do paralelepipedo"))
+largura = float(input("informe a largura do paralelepipedo"))
+
+volume = comprimento * altura * largura
+
+print(f"O volume do paralelpipedo equivale a {volume}")
