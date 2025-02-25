ab2dbf9b6ab940ef000d89c2826e9068a76b4f77
nicAmSarah <nicaciosarah085@gmail.com>
2025-02-24 23:56:31 -0300

    Ex01 - Lote01

---
+++ LT01.EstSeq01.py
@@ -0,0 +1,5 @@
+ladoQuadrado = int(input("Digite o lado do quadrado para calcular a area dele."))
+
+area = ladoQuadrado * ladoQuadrado
+
+print(f"A area do lado no valor de {ladoQuadrado} equivale á {area}")
