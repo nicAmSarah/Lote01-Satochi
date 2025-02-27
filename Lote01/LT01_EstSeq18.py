c8126a2f8258c961ca9b5730e25c229b465a4ce0
nicAmSarah <nicaciosarah085@gmail.com>
2025-02-26 22:14:04 -0300

    Ex18 - Lote01

---
+++ Lote01/LT01_EstSeq18.py
@@ -0,0 +1,9 @@
+num1= int(input("digite um numero"))
+num2= int(input("digite outro numero"))
+
+if num1 > num2:
+    diferenca = num1 - num2
+    print(diferenca)
+elif num1 < num2:
+    diferenca = num2 - num1
+    print(diferenca)
