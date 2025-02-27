9a598ca090ee2231056ebc2920f64d18d2cf0555
nicAmSarah <nicaciosarah085@gmail.com>
2025-02-26 21:40:07 -0300

    Ex14 - Lote01

---
+++ Lote01/LT01_EstSeq14.py
@@ -0,0 +1,6 @@
+angulo1 = float(input("Digite o numero do primeiro angulo a ser calculado do triangulo:"))
+angulo2 = float(input("Digite o numero do segundo angulo a ser calculado do triangulo:"))
+
+terceiroAngulo = 180 - (angulo1 + angulo2)
+
+print(terceiroAngulo)
