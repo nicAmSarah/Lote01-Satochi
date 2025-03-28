a54c769a0c07a87d02227ecf025ad7c1a561233c
nicAmSarah <nicaciosarah085@gmail.com>
2025-03-28 00:44:18 -0300

    Ex29 - Lote01

---
+++ Lote01/LT01_EstSeq29.py
@@ -0,0 +1,12 @@
+tipoInvestimento = int(input("Digite o tipo de conta que você ira investir\n [1] - Poupnça\n [2] - Renda Fixa\n"))
+valorInvestimento = float(input("Qual o valor que você deseja investir?"))
+
+if tipoInvestimento == 1 or tipoInvestimento == 2:
+    if tipoInvestimento == 1:
+        investimento = valorInvestimento + (valorInvestimento*0.03)
+        print(f"o valor de R${valorInvestimento} foi investido em 3%, totalizando {investimento}")
+    else:
+        investimento = valorInvestimento + (valorInvestimento*0.03)
+        print(f"o valor de R${valorInvestimento} foi investido em 5%, totalizando {investimento}")
+else:
+    print("não valido")
